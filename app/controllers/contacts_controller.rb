784b040be8ea44103a0fc15d7ceaef5bea8d5f55
Pushkar Jaltare <pushkar291191@gmail.com>
2017-02-01 22:16:56 +0000

    added comments for Contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,22 @@
 class ContactsController < ApplicationController
+    # GET request to /contact-us
     def new
         @contact = Contact.new
     end
     
+    #POST request to /contacts
     def create
+        #mass assignments of form fields into Contact object
         @contact = Contact.new(contact_params)
         if @contact.save
+            #store form fields into variables
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
+            #send email 
             ContactMailer.contact_email(name,email,body).deliver
-            
             flash[:success] = "Message Sent"
             redirect_to new_contact_path
-            
             
         else
             flash[:danger] = @contact.errors.full_messages.join(", ")
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    #validation on contacts forms
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
